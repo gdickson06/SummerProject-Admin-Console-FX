ea145a3e7fd833d62ce259344fc5cdcaf9907457
calum <calum@DESKTOP-E9DC0IF.home>
2017-07-28 15:27:44 +0100

    Attached ccs

--- Test1/src/application/Login.java
+++ Test1/src/application/Login.java
@@ -74,7 +74,7 @@ public class Login extends Application {
 
 		Scene scene = new Scene(grid, 800, 400);
 		primaryStage.setScene(scene);
-		scene.getStylesheets().add(Login.class.getResource("/Login.css").toExternalForm());
+		scene.getStylesheets().add(Login.class.getResource("Login.css").toExternalForm());
 		primaryStage.show();
 	}
 
